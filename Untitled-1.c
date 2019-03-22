833bbfab00e13cb044a2e2d8477cfd25f75d5c2a
cersosimof <cersosimo.facundo@gmail.com>
2019-03-22 11:45:02 -0300

    primer commit

---
+++ Untitled-1.c
@@ -0,0 +1,41 @@
+/* EJERCICIO 1.2 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*scanf ("%codigoFormato", &nombreVariableQueAlmacenaLaEntrada);*/
+
+int main()
+{
+int dia, mes, ano;
+printf("Ingrese dia: ");
+scanf("%d", &dia);
+printf("Ingrese mes: ");
+scanf("%d", &mes);
+printf("Ingrese ano: ");
+scanf("%d", &ano);
+printf("Estamos en el %d/%d/%d", dia, mes, ano);
+}
+
+// EJERCICIO 1.3
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*scanf ("%codigoFormato", &nombreVariableQueAlmacenaLaEntrada);*/
+
+int main()
+{
+int base, altura;
+
+printf("Indique base del triangulo: ");
+scanf("%d", &base);
+
+printf("Gracias, indique altura: a");
+scanf("%d", &altura);
+
+printf("La superficie es %d", base*altura/2);
+}
+
+//EJERCICIO 1.4
+
